22f7449928825b6bdc2688b098aa8c4eeebee80e
udaya018 <udaya.ramamurthy@gmail.com>
2023-03-01 23:40:01 +0530

    Chapter - 1 Inception

---
+++ App.js
@@ -0,0 +1,18 @@
+const heading1 = React.createElement(
+    "h1",
+    { id: "title" },
+    "Heading 1"
+  );
+  const heading2 = React.createElement(
+    "h2",
+    { id: "title" },
+    "Heading 2"
+  );
+  const container = React.createElement(
+    "div",
+    { id: "container" },
+    [heading1, heading2]
+  );
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  // Passing a react element inside root
+  root.render(container);
