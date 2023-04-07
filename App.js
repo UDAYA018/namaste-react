a4a8a8740bceedf67d4d60e90e20f81455673273
udaya018 <udaya.ramamurthy@gmail.com>
2023-04-08 01:53:55 +0530

    Chapter - 03 Laying the Foundation

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
-
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-    "h1",
-    { id: "title" },
-    "Heading 1 from Parcel"
+// const heading1 = React.createElement(
+//   "h1",
+//   { id: "title", key: "h1" },
+//   "Heading 1 from Parcel"
+// );
+// const heading2 = React.createElement(
+//   "h2",
+//   { id: "title", key: "h2" },
+//   "Heading 2"
+// );
+// const container = React.createElement("div", { id: "container" }, [
+//   heading1,
+//   heading2,
+// ]);
+
+const Title = () => (
+  <h1 id="title" key="h1">
+    Namaste React
+  </h1>
+);
+// Component Composition
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h1>H1 Tag</h1>
+      <h2>h2 tag</h2>
+    </div>
   );
-  const heading2 = React.createElement(
-    "h2",
-    { id: "title" },
-    "Heading 2"
-  );
-  const container = React.createElement(
-    "div",
-    { id: "container" },
-    [heading1, heading2]
-  );
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  // Passing a react element inside root
-  root.render(container);
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// Passing a react element inside root
+root.render(<HeaderComponent />);
